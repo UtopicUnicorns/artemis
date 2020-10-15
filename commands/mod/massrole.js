ac9fc9d0574d429ddbfaa94e66e11681c805d23b
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2020-10-15 19:15:55 +0200

    Donate

---
+++ commands/mod/massrole.js
@@ -0,0 +1,36 @@
+//Load modules
+const npm = require("../../modules/NPM.js");
+npm.npm();
+
+//load database
+dbinit = require("../../modules/dbinit.js");
+dbinit.dbinit();
+
+//start
+module.exports = {
+  category: `mod`,
+  name: "massrole",
+  description: "[mod] Give mentioned role to everyone",
+  explain: `By mentioning a role in this command you can give the selected role to everyone.
+  
+  Example usage: \`massrole @role\``,
+  async execute(message) {
+    //build prefix
+    const prefixstart = getGuild.get(message.guild.id);
+    const prefix = prefixstart.prefix;
+
+    //if user has perms
+    if (!message.member.permissions.has("KICK_MEMBERS")) return;
+
+    //update usage
+    usage = getUsage.get("massrole");
+    usage.number++;
+    setUsage.run(usage);
+
+    const Role = message.mentions.roles.first();
+
+    message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.add(Role))
+
+    message.channel.send(`Added role ${Role.name}`);
+  },
+};
