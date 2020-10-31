54a8d608515fd57bcb4ca1ec4126c6f4170260c9
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2020-10-31 08:41:40 +0100

    Donate

--- commands/fun/lenny.js
+++ commands/fun/lenny.js
@@ -11,7 +11,7 @@ module.exports = {
   category: `fun`,
   name: "lenny",
   description: "[fun] ( ͡° ͜ʖ ͡°)",
-  explain: `This command pulls adult content from a website, this command is only usable in channels that are marked NSFW!`,
+  explain: `( ͡° ͜ʖ ͡°)`,
   execute(message) {
     //build prefix
     const prefixstart = getGuild.get(message.guild.id);
@@ -22,68 +22,7 @@ module.exports = {
     usage.number++;
     setUsage.run(usage);
 
-    //check if nsfw
-    if (!message.channel.nsfw) return message.reply("( ͡° ͜ʖ ͡°)");
-
-    //define args
-    let args = message.content
-      .toLowerCase()
-      .slice(prefix.length + 6)
-      .split(" ");
-
-    //if no args
-    if (!args[0]) {
-      request(`https://aranym.com/artemis-categories`, (err, res, body) => {
-        //if err
-        if (!body) return message.reply("An error occured");
-
-        //split body
-        body = body.split("\n");
-
-        return message.reply(
-          `\`\`\`diff\n- Pick a category:\n\n+ ${body.join(`\n+ `)}\n\n\`\`\``
-        );
-      });
-    }
-
-    //if args[1]
-    if (args[1]) {
-      //build embed
-      const embed = new Discord.MessageEmbed().setImage(
-        `https://aranym.com/${args[0]}/${args[1]}.jpg`
-      );
-
-      //send embed
-      return message.channel.send({
-        embed: embed,
-      });
-    }
-
-    //request
-    request(`https://aranym.com/${args[0]}/filenames`, (err, res, body) => {
-      //if err
-      if (!body) return message.reply("Category not found!");
-
-      //sort body
-      body = body.split("\n");
-      body = body.slice(Math.max(body.length - 3, 0));
-      body = body[0].split(".");
-
-      //define number
-      number = body[0];
-
-      //define random
-      let num = Math.floor(Math.random() * number + 1);
-
-      //build embed
-      const embed = new Discord.MessageEmbed().setImage(
-        `https://aranym.com/${args[0]}/${num}.jpg`
-      );
-
-      //send embed
-      return message.channel.send({
-        embed: embed,
-      });
-    });
+    //yes
+    return message.reply("( ͡° ͜ʖ ͡°)");
   },
 };
