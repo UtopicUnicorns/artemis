661fbbdd35a232c46a7272ad29f9f44eec199ce7
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2020-10-31 09:48:42 +0100

    Donate

--- commands/mod/massrole.js
+++ commands/mod/massrole.js
@@ -11,28 +11,47 @@ module.exports = {
   category: `mod`,
   name: "massrole",
   description: "[mod] Give mentioned role to everyone",
-  explain: `By mentioning a role in this command you can give the selected role to everyone.
+  explain: `By giving this command a rolename you can add the role to every member in your server who does not have this role yet.
   
-  Example usage: \`massrole @role\``,
+  Example usage: \`massrole roleName\``,
   async execute(message) {
     //build prefix
     const prefixstart = getGuild.get(message.guild.id);
     const prefix = prefixstart.prefix;
 
     //if user has perms
-    if (!message.member.permissions.has("KICK_MEMBERS")) return message.reply("You do not have permissions to use this command!");
+    if (!message.member.permissions.has("KICK_MEMBERS"))
+      return message.reply("You do not have permissions to use this command!");
 
     //update usage
     usage = getUsage.get("massrole");
     usage.number++;
     setUsage.run(usage);
 
-    return message.reply("This command is temporarely disabled due to well I have no damn clue.. Something is wrong reeee!");
+    //args
+    let args = message.content.slice(prefix.length + 9);
 
-    const Role = message.mentions.roles.first();
+    //cache members
+    await message.guild.members.fetch();
 
-    message.guild.members.cache.filter(m => !m.user.bot).forEach(member => member.roles.add(Role))
+    //throw all members into an array
+    let array = await message.guild.members.cache.map((m) => m);
 
-    message.channel.send(`Added role ${Role.name}`);
+    //define role
+    const role = await message.guild.roles.cache.find((r) => r.name === args);
+
+    //if no role
+    if (!role) return message.reply("Role not found!");
+
+    message.reply("This may take a while, i'll let you know when I am done!");
+
+    for (let i of array)
+      if (!i.roles.cache.find((r) => r.id === role.id))
+        if (!i.user.bot)
+          await message.guild.members.cache.get(i.user.id).roles.add(role);
+
+    message.reply(
+      `Done, added role ${role.name} to all users who did not have this role!`
+    );
   },
 };
